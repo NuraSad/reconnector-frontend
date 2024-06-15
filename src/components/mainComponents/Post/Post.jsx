aeaf3be68759ff732378158b2745596377512ae9
revyrob <kayle.robson@gmail.com>
2024-06-14 22:55:50 -0700

    updating the styling for the posts within explore

--- src/components/mainComponents/Post/Post.jsx
+++ src/components/mainComponents/Post/Post.jsx
@@ -1,16 +1,21 @@
+import Avatar from "@mui/material/Avatar";
+import user1 from "../../../assets/user1.png";
+import "./Post.scss";
+
 export default function Post() {
   return (
-    <div>
-      <div>
-        <div>NAME</div>
-        <div>tags</div>
+    <div className="post">
+      <div className="post__user-info">
+        <Avatar alt="user" src={user1} />
+        <div className="post__user-info--name">NAME</div>
+        <div className="post__user-info--tags">tags</div>
       </div>
-      <div>
+      <div className="post__images">
         <img />
       </div>
-      <div>
-        <h2>title</h2>
-        <p>text</p>
+      <div className="post__info">
+        <h2 className="post__info--title">title</h2>
+        <p className="post__info--text">text</p>
       </div>
     </div>
   );
