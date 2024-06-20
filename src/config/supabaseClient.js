29eb4b234a0d56acc969ec9c765890084419ee62
angelatyk <angelatyk@gmail.com>
2024-06-20 02:13:33 -0400

    Setting up config files to connect to Supabase.

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,11 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  "https://chpjbgfwdsndnggcbtxd.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNocGpiZ2Z3ZHNuZG5nZ2NidHhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0MzUxNzgsImV4cCI6MjAzNDAxMTE3OH0._I67uX37v_FN4HzsftfVNr0X-HiPwedHe9vcSYw55tM"
-);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_ANON_KEY;
 
-//process.env.REACT_APP_SUPABASE_URL
-//process.env.REACT_APP_SUPABASE_ANON_KEY
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
