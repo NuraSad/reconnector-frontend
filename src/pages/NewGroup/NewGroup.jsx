4e2bf816dc8a08631f21f82b76b7541539b036c3
revyrob <kayle.robson@gmail.com>
2024-06-21 02:22:03 -0700

    finished the functions and styling for the right hand side

--- src/pages/NewGroup/NewGroup.jsx
+++ src/pages/NewGroup/NewGroup.jsx
@@ -6,7 +6,51 @@ function NewGroup() {
   //state for group name
   const [newGroup, setNewGroup] = useState({});
   const [toggleOnline, setToggleOnline] = useState(false);
-  const [togglePerson, setTogglePerson] = useState(true);
+  const [togglePerson, setTogglePerson] = useState(false);
+  const [selected, setSelected] = useState({});
+
+  function onSelect(e) {
+    let name = e.target.name;
+    setSelected((prevState) => ({
+      ...prevState,
+      [name]: !prevState[name],
+    }));
+    setNewGroup((prevState) => {
+      // Ensure dates is an array before proceeding
+      const currentDates = Array.isArray(prevState.dates)
+        ? prevState.dates
+        : [];
+
+      let updatedDates;
+      // Add the date if it doesn't exist
+      if (currentDates.some((date) => date === name)) {
+        // Remove the date if it already exists
+        updatedDates = currentDates.filter((date) => date !== name);
+      } else {
+        // Add the date if it doesn't exist
+        updatedDates = [...currentDates, name];
+      }
+
+      return {
+        ...prevState,
+        dates: updatedDates,
+      };
+    });
+
+    console.log(e.target.name);
+  }
+
+  //location street, city, country
+  function location(e) {
+    const { name, value } = e.target;
+    setNewGroup((prevState) => ({
+      ...prevState,
+      location: {
+        ...prevState.location,
+        [name]: value,
+      },
+    }));
+  }
 
   //set the group name in the newgroup object
   function groupNaming(e) {
@@ -51,6 +95,16 @@ function NewGroup() {
     }
   }
 
+  //set the description
+  function description(e) {
+    let description = e.target.value;
+    setNewGroup((prevState) => ({
+      ...prevState,
+      description: description,
+    }));
+  }
+  const buttonNames = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+
   console.log(newGroup);
   return (
     <section className="newGroup">
@@ -58,7 +112,12 @@ function NewGroup() {
       <div className="newGroup__col-1">
         <div className="newGroup__input">
           <label>What is this exciting new group called?</label>
-          <input type="text" name="groupName" onChange={groupNaming}></input>
+          <input
+            className="newGroup__input--tall"
+            type="text"
+            name="groupName"
+            onChange={groupNaming}
+          ></input>
         </div>
         <div className="newGroup__input">
           <label>How can people attend {newGroup.groupName}?</label>
@@ -67,7 +126,7 @@ function NewGroup() {
               textBtn={"In-person?"}
               bgColor={"#6c3ed64f"}
               fontSize={"16px"}
-              fontWeight={"Bold"}
+              fontWeight={"500"}
               textColor={"white"}
               height={"45px"}
               inputType="checkbox"
@@ -80,7 +139,7 @@ function NewGroup() {
               textBtn={"Online?"}
               bgColor={"#6c3ed64f"}
               fontSize={"16px"}
-              fontWeight={"Bold"}
+              fontWeight={"500"}
               textColor={"white"}
               height={"45px"}
               inputType="checkbox"
@@ -93,84 +152,59 @@ function NewGroup() {
         </div>
         <div className="newGroup__input">
           <label>When do people show up?</label>
-          <div className="newGroup__check">
-            <Btn
-              textBtn={"Sunday"}
-              bgColor={"#6c3ed64f"}
-              fontSize={"16px"}
-              fontWeight={"Bold"}
-              textColor={"white"}
-              height={"45px"}
-              onChange={online}
-              width={"205px"}
-              name="inperson"
-            />
-            <Btn
-              textBtn={"Monday"}
-              bgColor={"#6c3ed64f"}
-              fontSize={"16px"}
-              fontWeight={"Bold"}
-              textColor={"white"}
-              height={"45px"}
-              onChange={person}
-              width={"205px"}
-              name="online"
-            />
-            <Btn
-              textBtn={"Tuesday"}
-              bgColor={"#6c3ed64f"}
-              fontSize={"16px"}
-              fontWeight={"Bold"}
-              textColor={"white"}
-              height={"45px"}
-              onChange={online}
-              width={"205px"}
-              name="inperson"
-            />
-            <Btn
-              textBtn={"Wednesday"}
-              bgColor={"#6c3ed64f"}
-              fontSize={"16px"}
-              fontWeight={"Bold"}
-              textColor={"white"}
-              height={"45px"}
-              onChange={person}
-              width={"205px"}
-              name="online"
-            />
-            <Btn
-              textBtn={"Thursday"}
-              bgColor={"#6c3ed64f"}
-              fontSize={"16px"}
-              fontWeight={"Bold"}
-              textColor={"white"}
-              height={"45px"}
-              onChange={online}
-              width={"205px"}
-              name="inperson"
-            />
-            <Btn
-              textBtn={"Friday"}
-              bgColor={"#6c3ed64f"}
-              fontSize={"16px"}
-              fontWeight={"Bold"}
-              textColor={"white"}
-              height={"45px"}
-              onChange={person}
-              width={"205px"}
-              name="online"
-            />
-            <Btn
-              textBtn={"Saturday"}
-              bgColor={"#6c3ed64f"}
-              fontSize={"16px"}
-              fontWeight={"Bold"}
-              textColor={"white"}
-              height={"45px"}
-              onChange={person}
-              width={"205px"}
-              name="online"
-            />
+          <div className="newGroup__dates">
+            {buttonNames &&
+              buttonNames.map((btn, index) => (
+                <>
+                  <Btn
+                    key={index}
+                    textBtn={btn}
+                    bgColor={`${selected[btn] ? "#000" : "#D9D9D9"}`}
+                    fontSize={"16px"}
+                    fontWeight={"500"}
+                    textColor={selected[btn] ? "#FFF" : "#000"}
+                    height={"25px"}
+                    onClick={onSelect}
+                    width={"205px"}
+                    name={btn}
+                    padding={"2px 8px"}
+                  />
+                </>
+              ))}
+          </div>
+        </div>
+        <div className="newGroup__input">
+          <label>Where do people meet-up?</label>
+          <div className="newGroup__location">
+            <input
+              type="text"
+              name="address"
+              placeholder={"Street Address"}
+              onChange={location}
+            ></input>
+            <input
+              type="text"
+              name="city"
+              placeholder={"City"}
+              onChange={location}
+            ></input>
+            <input
+              type="text"
+              name="country"
+              placeholder={"Country"}
+              onChange={location}
+            ></input>
+          </div>
+        </div>
+        <div className="newGroup__input">
+          <label>Describe your event, tells others what to expect. </label>
+          <div className="newGroup__text">
+            <textarea
+              type="text"
+              name="descript"
+              placeholder={"This activity will be enjoyable..."}
+              onChange={description}
+            ></textarea>
           </div>
         </div>
       </div>
