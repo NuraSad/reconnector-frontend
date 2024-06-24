3583d96704523ef0b581b638ed70879e3c2c87b6
angelatyk <angelatyk@gmail.com>
2024-06-24 03:47:26 -0400

    Connect user information from database to Root.

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,147 +1,194 @@
-import {
-  Form,
-  Link,
-  NavLink,
-  Outlet,
-  redirect,
-  useLoaderData,
-  useNavigation,
-  useSubmit,
-} from "react-router-dom";
-import CardList from "../../components/mainComponents/CardList/CardList";
+import { useEffect, useState } from "react";
+import { Link, NavLink, Outlet } from "react-router-dom";
 import logo from "../../assets/icons/reconnect-logo.svg";
 import companyLogo from "../../assets/icons/starbacks-logo.png";
 import eventImage from "../../assets/running-club.jpg";
 import samantha from "../../assets/samantha.png";
+import CardList from "../../components/mainComponents/CardList/CardList";
 import supabase from "../../config/supabaseClient";
+import { getUserId } from "../../userUtils.js";
 import "./Root.scss";
 
 const events = [
-  {
-    event_id: "1",
-    date: "Monday @3:30pm",
-    name: "running sunday fun day",
-    attendees: 25,
-    src: eventImage,
-  },
-  {
-    event_id: "2",
-    date: "Friday @12:30pm",
-    name: "running sunday fun day",
-    attendees: 25,
-    src: eventImage,
-  },
-  {
-    event_id: "3",
-    date: "Sunday @8:30am",
-    name: "running sunday fun day",
-    attendees: 25,
-    src: eventImage,
-  },
+	{
+		event_id: "1",
+		date: "Monday @3:30pm",
+		name: "running sunday fun day",
+		attendees: 25,
+		src: eventImage,
+	},
+	{
+		event_id: "2",
+		date: "Friday @12:30pm",
+		name: "running sunday fun day",
+		attendees: 25,
+		src: eventImage,
+	},
+	{
+		event_id: "3",
+		date: "Sunday @8:30am",
+		name: "running sunday fun day",
+		attendees: 25,
+		src: eventImage,
+	},
 ];
 
 export default function Root() {
-  const user = {
-    name: "Samantha",
-    company: companyLogo,
-    src: samantha,
-    groups: 8,
-    points: 123,
-    streak: 2,
-  };
+	const [internalUserId, setInternalUserId] = useState(null);
+	const [user, setUser] = useState(null);
+	const [userCompany, setUserCompany] = useState(null);
+	const [userGroupsCount, setUserGroupsCount] = useState(0);
 
-  return (
-    <div className="container">
-      <nav>
-        <img className="root__logo" src={logo} />
-        <div className="user-info">
-          <div className="avatar-field">
-            <Link to="/profile">
-              <img id="avatar" src={user.src} alt={`${user.name}'s avatar`} />
-            </Link>
-            <img
+	useEffect(() => {
+		const fetchUserId = async () => {
+			const userId = await getUserId();
+			setInternalUserId(userId);
+		};
+
+		fetchUserId();
+	}, []);
+
+	useEffect(() => {
+		const getUser = async () => {
+			let { data: user_data, error: user_error } = await supabase.from("user").select().eq("id", internalUserId);
+
+			if (user_error) {
+				console.error("Error fetching user:", user_error.message);
+				return;
+			}
+
+			if (user_data && user_data.length > 0) {
+				setUser(user_data[0]);
+			}
+		};
+
+		if (internalUserId) {
+			getUser();
+		}
+	}, [internalUserId]);
+
+	useEffect(() => {
+		const getUserCompany = async () => {
+			let { data: user_company_data, error: user_company_error } = await supabase.from("company").select().eq("id", user.company_id);
+
+			if (user_company_error) {
+				console.error("Error fetching user:", user_company_error.message);
+				return;
+			}
+
+			if (user_company_data && user_company_data.length > 0) {
+				setUserCompany(user_company_data[0]);
+			}
+		};
+
+		const getUserGroupCount = async () => {
+			let { count } = await supabase.from("group_members").select("*", { count: "exact", head: true }).eq("user_id", user.id);
+
+			if (count) {
+				setUserGroupsCount(count);
+			}
+		};
+
+		if (user) {
+			getUserCompany();
+			getUserGroupCount();
+		}
+	}, [user]);
+
+	return (
+		user && (
+			<div className="container">
+				<nav>
+					<img className="root__logo" src={logo} />
+					<div className="user-info">
+						<div className="avatar-field">
+							<Link to="/profile">
+								<img id="avatar" src={user.avatar ? user.avatar : samantha} alt={`${user.first_name}'s avatar`} />
+							</Link>
+							<img
               id="company-logo"
-              src={user.company}
-              alt={`${user.name}'s company logo`}
+              src={userCompany && userCompany.logo ? userCompany.logo : companyLogo}
+              alt={`${user.first_name}'s company logo`}
             />
-          </div>
-          <h4 className="root__user">{user.name}</h4>
-          <div className="stats-field">
-            <div className="stat">
-              <p>&#128101;</p>
-              <p>{user.groups}</p>
-            </div>
-            <div className="stat">
-              <p>&#127775;</p>
-              <p>{user.points}</p>
-            </div>
-            <div className="stat">
-              <p>&#128293;</p>
-              <p>{user.streak}</p>
-            </div>
-          </div>
-        </div>
-        <ul className="root__navlink">
-          <li>
+						</div>
+						<h4 className="root__user">{user.first_name}</h4>
+						<div className="stats-field">
+							<div className="stat">
+								<p>&#128101;</p>
+								<p>{userGroupsCount}</p>
+							</div>
+							<div className="stat">
+								<p>&#127775;</p>
+								<p>{user.points ? user.points : 0}</p>
+							</div>
+							<div className="stat">
+								<p>&#128293;</p>
+								<p>{user.streak ? user.streak : 0}</p>
+							</div>
+						</div>
+					</div>
+					<ul className="root__navlink">
+						<li>
             <NavLink
               to="/explore"
               className={({ isActive, isPending }) =>
                 isActive ? "active" : isPending ? "pending" : ""
               }
             >
-              Explore
-            </NavLink>
-          </li>
-          <li>
+								Explore
+							</NavLink>
+						</li>
+						<li>
             <NavLink
               to="/groups"
               className={({ isActive, isPending }) =>
                 isActive ? "active" : isPending ? "pending" : ""
               }
             >
-              Groups
-            </NavLink>
-          </li>
-          <li>
+								Groups
+							</NavLink>
+						</li>
+						<li>
             <NavLink
               to="/calendar"
               className={({ isActive, isPending }) =>
                 isActive ? "active" : isPending ? "pending" : ""
               }
             >
-              Calendar
-            </NavLink>
-          </li>
-          <li>
+								Calendar
+							</NavLink>
+						</li>
+						<li>
             <NavLink
               to="/maps"
               className={({ isActive, isPending }) =>
                 isActive ? "active" : isPending ? "pending" : ""
               }
             >
-              Maps
-            </NavLink>
-          </li>
-          <li>
+								Maps
+							</NavLink>
+						</li>
+						<li>
             <NavLink
               to="/leaderboards"
               className={({ isActive, isPending }) =>
                 isActive ? "active" : isPending ? "pending" : ""
               }
             >
-              Leaderboards
-            </NavLink>
-          </li>
-          <li>
-            <Link onClick={() => supabase.auth.signOut()}>Logout</Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="outlet">
-        <Outlet />
-      </div>
+								Leaderboards
+							</NavLink>
+						</li>
+						<li>
+							<Link onClick={() => supabase.auth.signOut()}>Logout</Link>
+						</li>
+					</ul>
+				</nav>
+				<div className="outlet">
+					<Outlet />
+				</div>
 
-      <CardList events={events} title={"Upcoming Events"} />
-    </div>
-  );
+				<CardList events={events} title={"Upcoming Events"} />
+			</div>
+		)
+	);
 }
