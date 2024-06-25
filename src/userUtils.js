191249e5ec5973180d0637bf1d0fd8e01fc30409
angelatyk <angelatyk@gmail.com>
2024-06-25 01:24:06 -0400

    Added function to get authenticated user id.

--- src/userUtils.js
+++ src/userUtils.js
@@ -1,23 +1,29 @@
 import supabase from "./config/supabaseClient";
 
 export const getUserId = async () => {
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  const auth_user_id = user.id;
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
+	const auth_user_id = user.id;
 
-  let { data: user_data, user_error } = await supabase
-    .from("user")
-    .select("id")
-    .eq("auth_user_id", auth_user_id);
+	let { data: user_data, user_error } = await supabase.from("user").select("id").eq("auth_user_id", auth_user_id);
 
-  if (user_error) {
-    console.log("Could not retrieve logged in user id " + user_error);
-  }
+	if (user_error) {
+		console.log("Could not retrieve logged in user id " + user_error);
+	}
 
-  if (user_data) {
-    return user_data[0].id;
-  }
+	if (user_data) {
+		return user_data[0].id;
+	}
 
-  return null;
+	return null;
+};
+
+export const getAuthUserId = async () => {
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
+	const auth_user_id = user.id;
+
+	return auth_user_id;
 };
