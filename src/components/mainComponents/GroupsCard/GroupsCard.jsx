f8916c89cd107c6acfd8a51ea9684b19f4dd67e1
revyrob <kayle.robson@gmail.com>
2024-06-25 23:28:01 -0700

    get the participants in each group to show

--- src/components/mainComponents/GroupsCard/GroupsCard.jsx
+++ src/components/mainComponents/GroupsCard/GroupsCard.jsx
@@ -1,18 +1,71 @@
-import { useState } from "react";
 import "./GroupsCard.scss";
 import plus from "../../../assets/icons/icon_plus.svg";
-import ProfileIcons from "../../smallComponents/ProfileIcons/ProfileIcons";
-import listAvatars from "../../../data/listAvatars.json";
-import DateItem from "../../smallComponents/DateItem/DateItem";
+import { useEffect, useState } from "react";
+import supabase from "../../../config/supabaseClient";
+import Avatar from "@mui/material/Avatar";
+import AvatarGroup from "@mui/material/AvatarGroup";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function GroupsCard(props) {
   const navigate = useNavigate();
-
+  const [groupMembers, setGroupMembers] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
   //state for the avatars coming in from the data file which will come in from the database later
-  const [usersAvatar] = useState(listAvatars);
   //we need to be pulling the event information and the group information in here, does this come from the parent?
 
+  //query the users in this group
+  useEffect(() => {
+    const fetchGroupMembers = async () => {
+      const groupId = props.id;
+      if (!props.id) {
+        setFetchError("No group ID provided");
+        return;
+      }
+      const { data: groupMembersData, error: groupMembersError } =
+        await supabase
+          .from("group_members")
+          .select("user_id")
+          .eq("group_id", groupId);
+      if (groupMembersError) {
+        setFetchError("Could not fetch user details");
+        return;
+      }
+      if (groupMembersError) {
+        setFetchError("Could not fetch the group members");
+        return;
+      }
+
+      if (groupMembersData) {
+        // Extract user_ids
+        const userIds = groupMembersData.map((member) => member.user_id);
+        console.log(userIds);
+        // Fetch user details from users table
+        const { data: usersData, error: usersError } = await supabase
+          .from("user")
+          .select("first_name, avatar")
+          .in("id", userIds);
+
+        if (usersError) {
+          setFetchError("Could not fetch user details");
+          return;
+        }
+
+        if (usersData) {
+          // Combine the data
+          const combinedData = groupMembersData.map((member) => {
+            const user = usersData.find((user) => user.id === member.user_id);
+            console.log(user);
+            return { ...member, user };
+          });
+          // console.log(combinedData);
+          setGroupMembers(usersData);
+          setFetchError(null);
+        }
+      }
+    };
+
+    fetchGroupMembers();
+  }, [props.id]);
   return (
     <div
       className="groupCard"
@@ -22,11 +75,17 @@ export default function GroupsCard(props) {
       <div className="groupCard__wrapper">
         <div className="header">
           <div className="header__group-title">{props.groups__title}</div>
-          <ProfileIcons users={usersAvatar} />
+
+          <AvatarGroup max={4}>
+            {groupMembers &&
+              groupMembers.map((each) => (
+                <Avatar key={each.id} alt={each.first_name} src={each.avatar} />
+              ))}
+          </AvatarGroup>
         </div>
         <div className="dates">
-          <DateItem date="Mon" />
-          <DateItem date="Tue" />
+          {/* <DateItem date="Mon" />
+          <DateItem date="Tue" /> */}
         </div>
         <div className="image">
           <img src={props.groups__image} alt={props.groups__title} />
