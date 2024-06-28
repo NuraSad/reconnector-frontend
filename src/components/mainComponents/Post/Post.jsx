2a37523bcb0636f9d1f6d853683d637e9030e60d
henrybellman@gmail.com <henrybellman@gmail.com>
2024-06-27 21:09:58 -0700

    updates

--- src/components/mainComponents/Post/Post.jsx
+++ src/components/mainComponents/Post/Post.jsx
@@ -20,6 +20,7 @@ export default function Post({
   const [fetchError, setFetchError] = useState("");
   const [likeCount, setLikeCount] = useState(likes);
   const [thisAuthor, setThisAuthor] = useState({});
+  const [showFullText, setShowFullText] = useState(false);
   const [isLiked, setIsLiked] = useState(() => {
     // Initialize isLiked from session storage if it exists
     const savedIsLiked = sessionStorage.getItem(`isLiked_${id}`);
@@ -75,29 +76,14 @@ export default function Post({
     <div className="post">
       <div className="post__user-info">
         <div className="post__user-info--userWrap">
-          <Avatar alt={username} src={thisAuthor.avatar} />
-          <div className="post__user-info--nameWrap">
-            <h3 className="post__user-info--name">
-              {thisAuthor.first_name} {thisAuthor.last_name}
-            </h3>
-            {/* we don't have a username */}
-            {/* <h4 className="post__user-info--user">@{thisAuthor.username} </h4> */}
+          <div className="post__user-info-group">
+            <Avatar alt={username} src={thisAuthor.avatar} />
+            <div className="post__user-info--nameWrap">
+              <h3 className="post__user-info--name">
+                {thisAuthor.first_name} {thisAuthor.last_name}
+              </h3>
+            </div>
           </div>
-        </div>
-
-        <p className="post__user-info--tags">#{tag}</p>
-      </div>
-      <div className="post__images">
-        <img className="post__images--main" alt="main image" src={img1} />
-        <div className="post__images--col-2">
-          <div className="post__info--col-1">
-            <h2 className="post__info--title">{postTitle}</h2>
-            <p className="post__info--text">{postText}</p>
-          </div>
-        </div>
-      </div>
-      <div className="post__info">
-        <div className="post__info--col-2">
           <div className="post__info--col-2--wrapper">
             <p
               className="post__info--col-2--p"
@@ -112,6 +98,44 @@ export default function Post({
               src={heart}
               style={{ filter: isLiked ? "none" : "brightness(50%)" }}
             />
+          </div>
+        </div>
+      </div>
+      <div className="post__images">
+        <img className="post__images--main" alt="main image" src={img1} />
+        <div className="post__images--col-2">
+            <h2 className="post__info--title">{postTitle}</h2>
+          <div className="post__info--col-1">
+            {postText.length > 150 && !showFullText ? (
+              <>
+                <p className="post__info--text">
+                  {postText.slice(0, 150)}...
+                </p>
+                <div className="post__info--wrap">
+                <button
+                  className="post__info--read-more"
+                  onClick={() => setShowFullText(true)}
+                >
+                  Read More
+                </button>
+                <p className="post__user-info--tags">#{tag}</p>
+              </div>
+              </>
+            ) : (
+              <>
+                <p className="post__info--text">{postText}</p>
+                <div className="post__info--wrap">
+                {postText.length > 150 ?( 
+                  <button
+                    className="post__info--read-more"
+                    onClick={() => setShowFullText(false)}
+                  >
+                    Hide
+                  </button>): <div></div>}
+                  <p className="post__user-info--tags">#{tag}</p>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
